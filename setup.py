e45c5ecf6c21cb5186f495bf1f68783ec64c6398
Jernej Vivod <vivod.jernej@gmail.com>
2022-04-02 16:06:26 +0200

    [feat][#1] Implement reductions to SAT for the n-queens and k-clique problems

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sat-reductions',
+    version='0.1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Jernej Vivod',
+    author_email='vivod.jernej@gmail.com',
+    description='SAT reduction examples'
+)
